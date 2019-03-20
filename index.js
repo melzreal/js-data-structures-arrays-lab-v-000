e81559adaddcb23eb228bb96e71b606f15275af1
Melissa Real <melissa.real@hotmail.com>
2019-03-20 13:24:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
-  function drivers(){
-    const muskies = ["Milo", "Otis", "Garfield"];
-    return muskies;
-  }
+
+
+  const drivers = ["Milo", "Otis", "Garfield"];
+  return muskies;
